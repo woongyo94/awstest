e4f07b8a6a227f4716e61952750be5cde3038cb3
oolralra <oolralra@gmail.com>
2022-03-30 17:24:07 +0900

    Update server_stop.sh

--- scripts/server_stop.sh
+++ scripts/server_stop.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-sudo rm -rf /home/ec2-user/tomcat/webapps/ROOT/*.jsp
+#sudo rm -rf /home/ec2-user/tomcat/webapps/ROOT/*.jsp
 sudo chmod +x /home/ec2-user/tomcat/bin/./shutdown.sh
 sudo /home/ec2-user/tomcat/bin/./shutdown.sh
