eadcc10aeff948738858fb635248ce9884b10947
oolralra <oolralra@gmail.com>
2022-06-20 16:06:32 +0900

    Update server_start.sh

--- scripts/server_start.sh
+++ scripts/server_start.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-sudo chmod +x /home/ec2-user/tomcat/bin/./startup.sh
-sudo /home/ec2-user/tomcat/bin/./startup.sh
+#sudo chmod +x /home/ec2-user/tomcat/bin/./startup.sh
+#sudo /home/ec2-user/tomcat/bin/./startup.sh
+cat index.html
